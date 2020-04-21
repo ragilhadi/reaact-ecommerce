9b62ef69c0a54363aac74883cae9b1ebeb698944
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-21 18:08:52 +0700

    Create Menu and Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./page/Homepage/Homepage";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route />
+        </Switch>
+      </BrowserRouter>
       <Homepage />
     </div>
   );
