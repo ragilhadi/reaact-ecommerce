a4c3942be6de33f53d0d445fc6675f3bc321d582
ragilhadi <ragilprasetyo310@gmail.com>
2020-04-22 19:11:31 +0700

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 import "./App.css";
 import Homepage from "./page/Homepage/Homepage";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Shop from "./page/Shop/Shop";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route />
-        </Switch>
+        <React.Fragment>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Homepage} />
+            <Route path="/shop" exact component={Shop} />
+          </Switch>
+        </React.Fragment>
       </BrowserRouter>
-      <Homepage />
     </div>
   );
 }
